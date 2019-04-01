d31f7ff6ff00ccdf83dc583993facbddeea40559
XxRafael09xX <38251594+XxRafael09xX@users.noreply.github.com>
2019-04-01 01:28:17 -0300

    Add files via upload

---
+++ Perfil de Filme/Programa/Form1.Designer.vb
@@ -0,0 +1,359 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Bob = New System.Windows.Forms.RadioButton()
+        Me.Mort = New System.Windows.Forms.RadioButton()
+        Me.Manute = New System.Windows.Forms.RadioButton()
+        Me.Dwight = New System.Windows.Forms.RadioButton()
+        Me.Eva = New System.Windows.Forms.RadioButton()
+        Me.Wallenquist = New System.Windows.Forms.RadioButton()
+        Me.Gail = New System.Windows.Forms.RadioButton()
+        Me.Wendy = New System.Windows.Forms.RadioButton()
+        Me.Miho = New System.Windows.Forms.RadioButton()
+        Me.Senador = New System.Windows.Forms.RadioButton()
+        Me.Johnny = New System.Windows.Forms.RadioButton()
+        Me.Nancy = New System.Windows.Forms.RadioButton()
+        Me.Marv = New System.Windows.Forms.RadioButton()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Bob)
+        Me.GroupBox1.Controls.Add(Me.Mort)
+        Me.GroupBox1.Controls.Add(Me.Manute)
+        Me.GroupBox1.Controls.Add(Me.Dwight)
+        Me.GroupBox1.Controls.Add(Me.Eva)
+        Me.GroupBox1.Controls.Add(Me.Wallenquist)
+        Me.GroupBox1.Controls.Add(Me.Gail)
+        Me.GroupBox1.Controls.Add(Me.Wendy)
+        Me.GroupBox1.Controls.Add(Me.Miho)
+        Me.GroupBox1.Controls.Add(Me.Senador)
+        Me.GroupBox1.Controls.Add(Me.Johnny)
+        Me.GroupBox1.Controls.Add(Me.Nancy)
+        Me.GroupBox1.Controls.Add(Me.Marv)
+        Me.GroupBox1.Location = New System.Drawing.Point(246, 0)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(131, 438)
+        Me.GroupBox1.TabIndex = 1
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Personagens"
+        '
+        'Bob
+        '
+        Me.Bob.AutoSize = True
+        Me.Bob.Location = New System.Drawing.Point(5, 361)
+        Me.Bob.Name = "Bob"
+        Me.Bob.Size = New System.Drawing.Size(44, 17)
+        Me.Bob.TabIndex = 12
+        Me.Bob.TabStop = True
+        Me.Bob.Text = "Bob"
+        Me.Bob.UseVisualStyleBackColor = True
+        '
+        'Mort
+        '
+        Me.Mort.AutoSize = True
+        Me.Mort.Location = New System.Drawing.Point(5, 330)
+        Me.Mort.Name = "Mort"
+        Me.Mort.Size = New System.Drawing.Size(46, 17)
+        Me.Mort.TabIndex = 11
+        Me.Mort.TabStop = True
+        Me.Mort.Text = "Mort"
+        Me.Mort.UseVisualStyleBackColor = True
+        '
+        'Manute
+        '
+        Me.Manute.AutoSize = True
+        Me.Manute.Location = New System.Drawing.Point(5, 206)
+        Me.Manute.Name = "Manute"
+        Me.Manute.Size = New System.Drawing.Size(61, 17)
+        Me.Manute.TabIndex = 9
+        Me.Manute.TabStop = True
+        Me.Manute.Text = "Manute"
+        Me.Manute.UseVisualStyleBackColor = True
+        '
+        'Dwight
+        '
+        Me.Dwight.AutoSize = True
+        Me.Dwight.Location = New System.Drawing.Point(5, 144)
+        Me.Dwight.Name = "Dwight"
+        Me.Dwight.Size = New System.Drawing.Size(109, 17)
+        Me.Dwight.TabIndex = 8
+        Me.Dwight.TabStop = True
+        Me.Dwight.Text = "Dwight McCardhy"
+        Me.Dwight.UseVisualStyleBackColor = True
+        '
+        'Eva
+        '
+        Me.Eva.AutoSize = True
+        Me.Eva.Location = New System.Drawing.Point(5, 175)
+        Me.Eva.Name = "Eva"
+        Me.Eva.Size = New System.Drawing.Size(76, 17)
+        Me.Eva.TabIndex = 10
+        Me.Eva.TabStop = True
+        Me.Eva.Text = "Eva Green"
+        Me.Eva.UseVisualStyleBackColor = True
+        '
+        'Wallenquist
+        '
+        Me.Wallenquist.AutoSize = True
+        Me.Wallenquist.Location = New System.Drawing.Point(5, 392)
+        Me.Wallenquist.Name = "Wallenquist"
+        Me.Wallenquist.Size = New System.Drawing.Size(80, 17)
+        Me.Wallenquist.TabIndex = 7
+        Me.Wallenquist.TabStop = True
+        Me.Wallenquist.Text = "Wallenquist"
+        Me.Wallenquist.UseVisualStyleBackColor = True
+        '
+        'Gail
+        '
+        Me.Gail.AutoSize = True
+        Me.Gail.Location = New System.Drawing.Point(5, 237)
+        Me.Gail.Name = "Gail"
+        Me.Gail.Size = New System.Drawing.Size(43, 17)
+        Me.Gail.TabIndex = 6
+        Me.Gail.TabStop = True
+        Me.Gail.Text = "Gail"
+        Me.Gail.UseVisualStyleBackColor = True
+        '
+        'Wendy
+        '
+        Me.Wendy.AutoSize = True
+        Me.Wendy.Location = New System.Drawing.Point(5, 299)
+        Me.Wendy.Name = "Wendy"
+        Me.Wendy.Size = New System.Drawing.Size(101, 17)
+        Me.Wendy.TabIndex = 5
+        Me.Wendy.TabStop = True
+        Me.Wendy.Text = "Wendy e Goldie"
+        Me.Wendy.UseVisualStyleBackColor = True
+        '
+        'Miho
+        '
+        Me.Miho.AutoSize = True
+        Me.Miho.Location = New System.Drawing.Point(5, 268)
+        Me.Miho.Name = "Miho"
+        Me.Miho.Size = New System.Drawing.Size(48, 17)
+        Me.Miho.TabIndex = 4
+        Me.Miho.TabStop = True
+        Me.Miho.Text = "Miho"
+        Me.Miho.UseVisualStyleBackColor = True
+        '
+        'Senador
+        '
+        Me.Senador.AutoSize = True
+        Me.Senador.Location = New System.Drawing.Point(5, 113)
+        Me.Senador.Name = "Senador"
+        Me.Senador.Size = New System.Drawing.Size(97, 17)
+        Me.Senador.TabIndex = 3
+        Me.Senador.TabStop = True
+        Me.Senador.Text = "Senador Roark"
+        Me.Senador.UseVisualStyleBackColor = True
+        '
+        'Johnny
+        '
+        Me.Johnny.AutoSize = True
+        Me.Johnny.Location = New System.Drawing.Point(5, 82)
+        Me.Johnny.Name = "Johnny"
+        Me.Johnny.Size = New System.Drawing.Size(59, 17)
+        Me.Johnny.TabIndex = 2
+        Me.Johnny.TabStop = True
+        Me.Johnny.Text = "Johnny"
+        Me.Johnny.UseVisualStyleBackColor = True
+        '
+        'Nancy
+        '
+        Me.Nancy.AutoSize = True
+        Me.Nancy.Location = New System.Drawing.Point(5, 51)
+        Me.Nancy.Name = "Nancy"
+        Me.Nancy.Size = New System.Drawing.Size(100, 17)
+        Me.Nancy.TabIndex = 1
+        Me.Nancy.TabStop = True
+        Me.Nancy.Text = "Nancy Callahan"
+        Me.Nancy.UseVisualStyleBackColor = True
+        '
+        'Marv
+        '
+        Me.Marv.AutoSize = True
+        Me.Marv.Location = New System.Drawing.Point(5, 20)
+        Me.Marv.Name = "Marv"
+        Me.Marv.Size = New System.Drawing.Size(49, 17)
+        Me.Marv.TabIndex = 0
+        Me.Marv.TabStop = True
+        Me.Marv.Text = "Marv"
+        Me.Marv.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(417, 12)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "Ficha"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(419, 396)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 3
+        Me.Button2.Text = "Fechar"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(419, 367)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 4
+        Me.Button3.Text = "Créditos"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.TextBox1)
+        Me.GroupBox2.Controls.Add(Me.Label1)
+        Me.GroupBox2.Controls.Add(Me.PictureBox2)
+        Me.GroupBox2.Dock = System.Windows.Forms.DockStyle.Right
+        Me.GroupBox2.Location = New System.Drawing.Point(523, 0)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(247, 438)
+        Me.GroupBox2.TabIndex = 5
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Detalhes"
+        '
+        'Label1
+        '
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(7, 20)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(113, 141)
+        Me.Label1.TabIndex = 2
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(409, 52)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(94, 17)
+        Me.CheckBox1.TabIndex = 6
+        Me.CheckBox1.Text = "Exibir ao clicar"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox1.Location = New System.Drawing.Point(10, 227)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.TextBox1.Size = New System.Drawing.Size(231, 205)
+        Me.TextBox1.TabIndex = 3
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.Location = New System.Drawing.Point(126, 20)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(115, 141)
+        Me.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox2.TabIndex = 1
+        Me.PictureBox2.TabStop = False
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Dock = System.Windows.Forms.DockStyle.Left
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(0, 0)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(240, 438)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ButtonFace
+        Me.ClientSize = New System.Drawing.Size(770, 438)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.ShowIcon = False
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "O Perfil dos Personagens de Sin City"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents Bob As System.Windows.Forms.RadioButton
+    Friend WithEvents Mort As System.Windows.Forms.RadioButton
+    Friend WithEvents Manute As System.Windows.Forms.RadioButton
+    Friend WithEvents Dwight As System.Windows.Forms.RadioButton
+    Friend WithEvents Eva As System.Windows.Forms.RadioButton
+    Friend WithEvents Wallenquist As System.Windows.Forms.RadioButton
+    Friend WithEvents Gail As System.Windows.Forms.RadioButton
+    Friend WithEvents Wendy As System.Windows.Forms.RadioButton
+    Friend WithEvents Miho As System.Windows.Forms.RadioButton
+    Friend WithEvents Senador As System.Windows.Forms.RadioButton
+    Friend WithEvents Johnny As System.Windows.Forms.RadioButton
+    Friend WithEvents Nancy As System.Windows.Forms.RadioButton
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents GroupBox2 As System.Windows.Forms.GroupBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents PictureBox2 As System.Windows.Forms.PictureBox
+    Friend WithEvents CheckBox1 As System.Windows.Forms.CheckBox
+    Public WithEvents Marv As RadioButton
+    Friend WithEvents TextBox1 As TextBox
+End Class
---
+++ Perfil de Filme/Programa/Form1.vb
@@ -0,0 +1,216 @@
+﻿
+Public Class Form1
+
+    Sub Marvs()
+        If Marv.Checked Then
+            PictureBox2.Image = My.Resources.marv
+            Label1.Text = "O retorno de Marv em Sin City – A Dama Fatal"
+            TextBox1.Text = "Em Sin City: A Dama Fatal, Mickey Rourke, interpreta mais uma vez o herói Marv. Com aparência amedrontadora e comportamento incontrolável, assim como no filme Sin City: Cidade do Pecado, Marv irá fazer de tudo para proteger seus amigos das maldades que cercam a cidade. Superprotetor das mulheres, ele irá ajudar Nancy Callahan em 'Nancy’s Last Dance'."
+        End If
+
+    End Sub
+
+    Sub nancys()
+        If Nancy.Checked Then
+            PictureBox2.Image = My.Resources.nancy
+            Label1.Text = "Nancy Callahan jura vingar a morte de Hartigan em Sin City: A Dama Fatal"
+            TextBox1.Text = "Nancy Callahan, interpretada por Jéssica Alba, personagem principal de ‘Bastardo Amarelo’, uma das histórias que compõe Sin City: Cidade do Pecado, dá continuidade a sua vingança em Sin City: A Dama Fatal após Hartigan sacrificar a própria vida para protegê-la. Nancy foi uma das personagens que mais se transformou durante a história, deixando de ser uma menininha inocente e passando a enfrentar os mais temidos vilões em Sin City: A Dama Fatal."
+        End If
+    End Sub
+
+    Sub Johnnys()
+        If Johnny.Checked Then
+            PictureBox2.Image = My.Resources.johnny
+            Label1.Text = "Dando sequência as histórias que compõe Sin Ciy – A Dama Fatal Gordon Levitt aparece em 'A Long, Bad Night'"
+            TextBox1.Text = "Johnny é um dos novos personagens de Sin City: A Dama Fatal, interpretado pelo ator Gordon Levitt, criado por Frank Miller especialmente para o filme. Johnny chega a Sin City e logo se dirige ao Kadie’s Club, onde consegue gerar uma grande quantia em caça-níqueis, antes de encontrar o temível Senador Roark. Diferente de qualquer história do primeiro filme, Miller, desenvolveu um personagem inédito sem se basear em nenhuma outra graphic novel."
+        End If
+    End Sub
+
+    Sub Senadors()
+        If Senador.Checked Then
+            PictureBox2.Image = My.Resources.roark
+            Label1.Text = "O maldoso Senador Roark promete atormentar ainda mais a vida dos habitantes de Sin City, em Sin City: A Dama Fatal "
+            TextBox1.Text = "Assim como no primeiro filme Sin City: Cidade do Pecado, o Senador Roark aparece em outras duas histórias 'The Long, Bad Night' e 'Nancy’s Last Dance' no novo Sin City: A Dama Fatal. Roark continua sendo o homem mais poderoso de Sin City, e promete atormentar a vida do novato Johnny e da sedutora Nancy, que busca vingar a morte de Hartigan."
+        End If
+    End Sub
+
+    Sub Dwights()
+        If Dwight.Checked Then
+            PictureBox2.Image = My.Resources.dwight
+            Label1.Text = "Dwight McCarthy retorna a Sin City interpretado por Josh Brolin"
+            TextBox1.Text = "O filme revela o porque do personagem reconstruir o rosto, que aparece completamente diferente de como o conhecemos no primeiro filme, Sin City: A Cidade do Pecado. Após abandonar Dwight, Ava Lord, se casa com um dos homens mais ricos de Sin City. Arrasado, ele passa a ter constantes ataques de raiva. Em Sin City: A Dama Fatal, Ava retorna bem no momento em que ele está conseguindo se reerguer."
+        End If
+    End Sub
+
+    Sub Evas()
+        If Eva.Checked Then
+            PictureBox2.Image = My.Resources.ava
+            Label1.Text = "Eva Green interpreta a Femme Fatale Ava Lord em Sin City: A Dama Fatal"
+            TextBox1.Text = "A femme fatale Ava Lord, convence boa parte dos personagens de Sin City: A Dama Fatal a realizar seus desejos. Com sua bela aparência, pele branca, olhos verdes e cabelos pretos, Ava, deixa qualquer homem aos seus pés. No filme, Ava implora que Dwight a proteja dos abusos que sofre nas mãos de seu marido e seu maligno braço-direito, Manute."
+        End If
+    End Sub
+
+    Sub Manutes()
+        If Manute.Checked Then
+            PictureBox2.Image = My.Resources.manute
+            Label1.Text = "Sin City – A Dama Fatal traz Manute como servo fiel de Ava Lord"
+            TextBox1.Text = "O misterioso Manute aparece em Sin City: A Dama Fatal como o guarda-costas da poderosa Ava Lord. Muitas histórias a respeito do passado de Manute serão reveladas, uma delas se refere a deformidade no olho do personagem. O fiel protetor do senhor do crime em Sin City: Cidade do Pecado irá fazer de tudo por Ava Lord. Manute é interpretado por Dennis Haysbert, que substitui Michael Clarke Duncan, falecido logo após o início das filmagens."
+        End If
+    End Sub
+
+    Sub Gails()
+        If Gail.Checked Then
+            PictureBox2.Image = My.Resources.gail
+            Label1.Text = "A guerreira amazona Gail tem como missão proteger Dwight que corre perigo em Sin City: A Dama Fatal"
+            TextBox1.Text = "Rosario Dawson interpreta Gail, uma guerreira amazona que lidera uma gangue de prostitutas assassinas em Old Town, o distrito mais perigoso de Sin City. Seu único ponto fraco é a relação que mantém com Dwight McCarthy, que após se ausentar de Sin City, retorna a Old Town pedindo ajuda. Gail tem a missão de defendê-lo até a morte"
+        End If
+    End Sub
+
+    Sub Mihos()
+        If Miho.Checked Then
+            PictureBox2.Image = My.Resources.miho
+            Label1.Text = "Guerreira oriental Miho faz justiça com as próprias mãos "
+            TextBox1.Text = "A corajosa Miho interpretada por Jamie Chung é uma exímia guerreira. Seus inimigos nunca estão preparados para enfrentar seus silenciosos golpes de espada. Devon Aoki, atriz que interpretou Miho no primeiro filme, teve que ser substituída por estar grávida. Miho acaba enfrentando todos que aparecem em seu caminho."
+        End If
+    End Sub
+
+    Sub Wendys()
+        If Wendy.Checked Then
+            PictureBox2.Image = My.Resources.wendy
+            Label1.Text = "Marv tenta desvendar o assassinato de Goldie em Sin City: A Dama Fatal"
+            TextBox1.Text = "Jaime King, retorna a Sin City, para interpretar as prostitutas gêmeas de Old Town, Wendy e Goldie. No primeiro filme, Marv acorda e encontra Goldie morta ao seu lado, levando-o a vingar a sua morte. Em Sin City: A Dama Fatal, é mostrado mais sobre a relação de Goldie com sua irmã."
+        End If
+    End Sub
+
+    Sub Morts()
+        If Mort.Checked Then
+            PictureBox2.Image = My.Resources.mort
+            Label1.Text = "Mort entrega-se a luxuria após conhecer Ava Lord em A Dama Fatal"
+            TextBox1.Text = "Um dos poucos policiais honestos de Sin City, Mort, acaba se rendendo aos encantos de Ava ao investigar a morte de seu marido. Mort aparece no filme como um policial casado e fiel incapaz de cometer qualquer delito."
+        End If
+    End Sub
+
+    Sub Bobs()
+        If Bob.Checked Then
+            PictureBox2.Image = My.Resources.bob
+            Label1.Text = "Bob interpreta um típico policial de Sin City, abusando dos privilégios que a função oferece"
+            TextBox1.Text = "Bob é um policial que abusa do poder e dos privilégios sua profissão em Sin City: A Dama Fatal. Com intuito de proteger seu parceiro Mort, ele o alerta sobre os perigos de se envolver com Ava Lord, uma misteriosa femme fatale com o poder de deixar qualquer homem aos seus pés."
+        End If
+    End Sub
+
+    Sub Wallenquists()
+        If Wallenquist.Checked Then
+            PictureBox2.Image = My.Resources.wallenquist
+            Label1.Text = "Stacy Keach Interpreta o chefe da máfia Wallenquist em Sin City"
+            TextBox1.Text = "O chefe da máfia Wallenquist, interpretado por Stacy Keach, retrata o lado grotesco de um homem que pertence ao lado escuro da força. Um personagem friamente mau, mas que tem um fraco por mulheres bonitas."
+        End If
+    End Sub
+
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Marvs()
+        nancys()
+        Johnnys()
+        Senadors()
+        Dwights()
+        Evas()
+        Manutes()
+        Gails()
+        Mihos()
+        Wendys()
+        Morts()
+        Bobs()
+        Wallenquists()
+
+    End Sub
+
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        End
+    End Sub
+
+    Private Sub Marv_CheckedChanged(sender As Object, e As EventArgs) Handles Marv.CheckedChanged
+        If CheckBox1.Checked Then
+            Marvs()
+        End If
+    End Sub
+
+    Private Sub Nancy_Click(sender As Object, e As EventArgs) Handles Nancy.Click
+        If CheckBox1.Checked Then
+            nancys()
+        End If
+    End Sub
+
+    Private Sub Johnny_Click(sender As Object, e As EventArgs) Handles Johnny.Click
+        If CheckBox1.Checked Then
+            Johnnys()
+        End If
+    End Sub
+
+    Private Sub Senador_Click(sender As Object, e As EventArgs) Handles Senador.Click
+        If CheckBox1.Checked Then
+            Senadors()
+        End If
+    End Sub
+
+    Private Sub Dwight_Click(sender As Object, e As EventArgs) Handles Dwight.Click
+        If CheckBox1.Checked Then
+            Dwights()
+        End If
+    End Sub
+
+    Private Sub Eva_Click(sender As Object, e As EventArgs) Handles Eva.Click
+        If CheckBox1.Checked Then
+            Evas()
+        End If
+    End Sub
+
+    Private Sub Manute_Click(sender As Object, e As EventArgs) Handles Manute.Click
+        If CheckBox1.Checked Then
+            Manutes()
+        End If
+    End Sub
+
+    Private Sub Gail_Click(sender As Object, e As EventArgs) Handles Gail.Click
+        If CheckBox1.Checked Then
+            Gails()
+        End If
+    End Sub
+
+    Private Sub Miho_Click(sender As Object, e As EventArgs) Handles Miho.Click
+        If CheckBox1.Checked Then
+            mihos()
+        End If
+    End Sub
+
+    Private Sub Wendy_Click(sender As Object, e As EventArgs) Handles Wendy.Click
+        If CheckBox1.Checked Then
+            Wendys()
+        End If
+    End Sub
+
+    Private Sub Mort_Click(sender As Object, e As EventArgs) Handles Mort.Click
+        If CheckBox1.Checked Then
+            Morts()
+        End If
+    End Sub
+
+    Private Sub Bob_Click(sender As Object, e As EventArgs) Handles Bob.Click
+        If CheckBox1.Checked Then
+            Bobs()
+        End If
+    End Sub
+
+    Private Sub Wallenquist_Click(sender As Object, e As EventArgs) Handles Wallenquist.Click
+        If CheckBox1.Checked Then
+            Wallenquists()
+        End If
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim f As Form2 = New Form2
+        f.Show()
+    End Sub
+End Class
